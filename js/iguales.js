fd1ba2829547cadb9b4173e43874ec249a825b9e
Jairo Serrano <jairo.serrano@gmail.com>
2012-10-09 15:08:50 -0500

    Funciona y muestra la información sobre:  - Intentos  - Aciertos  - Colorea los cuadros

--- js/iguales.js
+++ js/iguales.js
@@ -1,78 +1,55 @@
+/**
+ * tempA es la casilla primera pulsada
+ * tempB es la casilla segunda pulsada
+ * ganador es true si existe un ganador
+ * 
+ */
 var tempA;
 var tempB;
 var ganador;
+var ganadas=0;
+var intentos=0;
 
 
 $(".igboton").click(function() {
 
-    if (tempA != null && tempB != null){
-
-        if(ganador!=true){
-            tempA.removeClass("btn-danger");
-            tempB.removeClass("btn-danger");
-
-            tempA.addClass("btn-primary");
-            tempB.addClass("btn-primary");
-
-            tempA.text("");
-            tempB.text("");
-
-            tempA   = null;
-            tempB   = null;
-            ganador = false;
-        }else{
-
-        }
-    }
-
     if (tempA==null){
-
         tempA = $(this);
-        $(this).removeClass("btn-primary");
-        $(this).addClass("btn-warning");
-        $(this).text($(this).val());
-
-    //alert("Toma otro botón");
-    }else{
-        if($(this).val()==tempA.val() && $(this)==tempA){
-
-            $(this).text($(this).val());
-
-            $(this).removeClass("btn-warning");
-            tempA.removeClass("btn-warning");
-
-            $(this).addClass("btn-success");
-            tempA.addClass("btn-success");
-
-            ganador = true;
-
-        }else{
-
-            tempB = $(this);
-            tempB.text(tempB.val());
-
-            tempB.addClass("btn-primary");
-            tempB.removeClass("btn-warning");
+        tempA.text(tempA.val());
+        console.log("asignada ficha 1");
+        tempA.removeClass("btn-primary").addClass("btn-warning");
+    }else if(tempA!=null & tempB==null){
+        tempB = $(this);
+        tempB.text(tempB.val());
+        console.log("asignada ficha 2");
+        tempB.removeClass("btn-primary").addClass("btn-warning");
+        setTimeout (function (){
+            if(tempA.val()==tempB.val()){
+                ganador=true;
+                console.log("hay un ganador");
+                tempA.removeClass("btn-warning").addClass("btn-success");
+                tempB.removeClass("btn-warning").addClass("btn-success");
+                ganadas++;
+                console.log("Ganadas: "+ganadas);
+            }else{
+                ganador=false;
+                console.log("No hay ganador");
+                tempA.removeClass("btn-warning").addClass("btn-primary");
+                tempB.removeClass("btn-warning").addClass("btn-primary");
+                tempA.text("");
+                tempB.text("");
+                intentos++;
+                console.log("Intentos: "+intentos);
+            }
+            tempA=null;
+            tempB=null;
+        }, 200 );
             
-            tempA.addClass("btn-primary");
-            tempA.removeClass("btn-warning");
-
-            tempB = $(this);
-            tempB.addClass("btn-danger").fadeIn(1);
-            $(this).addClass("btn-danger");
-
-        //alert("De nuevo");
-        }
-        
     }
+    $("#ganadas").text("Aciertos: "+ganadas.toString());
+    $("#intentos").text("Intentos: "+intentos.toString());
+    
 });
-
-
-
-
-
-
-
 
 
 
